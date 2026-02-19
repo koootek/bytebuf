dff7625ac6a0dd8c0ad31b3650bfe2ce2965ee34
koootek <190116302+koootek@users.noreply.github.com>
2026-02-19 19:26:08 +0100

    Derive: Fix type parsing

--- bytebuf_derive/src/lib.rs
+++ bytebuf_derive/src/lib.rs
@@ -1,13 +1,13 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Data, DeriveInput, Field, Fields, parse_macro_input};
+use syn::{Data, DeriveInput, Field, Fields, parse_macro_input, spanned::Spanned};
 
 #[proc_macro_derive(IntoBytes)]
 pub fn derive_into_bytes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
-    let fields = match input.data {
+    let mut fields = match input.data {
         Data::Struct(s) => match s.fields {
             Fields::Named(named) => named.named.iter().cloned().collect::<Vec<Field>>(),
             Fields::Unnamed(_) | Fields::Unit => vec![],
@@ -18,11 +18,25 @@ pub fn derive_into_bytes(input: TokenStream) -> TokenStream {
                 .into();
         }
     };
-    let into_bytes_fields = fields.iter().map(|f| {
+    let into_bytes_fields = fields.iter_mut().map(|f| {
         let ident = f.ident.as_ref().unwrap();
-        let ty = &f.ty;
-        quote! {
-            #ty::into_bytes(self.#ident, buf)
+        let ty = &mut f.ty;
+        let span = ty.span();
+        match ty {
+            syn::Type::Path(syn::TypePath { qself: None, path }) => {
+                if let Some(last) = path.segments.last_mut()
+                    && let syn::PathArguments::AngleBracketed(ab) = &mut last.arguments
+                    && ab.colon2_token.is_none()
+                {
+                    ab.colon2_token = Some(syn::Token![::](span));
+                }
+                quote! {
+                    #ty::into_bytes(self.#ident, buf)
+                }
+            }
+            _ => quote! {
+                #ty::into_bytes(self.#ident, buf)
+            },
         }
     });
     TokenStream::from(quote! {
@@ -38,7 +52,7 @@ pub fn derive_into_bytes(input: TokenStream) -> TokenStream {
 pub fn derive_from_bytes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
-    let fields = match input.data {
+    let mut fields = match input.data {
         Data::Struct(s) => match s.fields {
             Fields::Named(named) => named.named.iter().cloned().collect::<Vec<Field>>(),
             Fields::Unnamed(_) | Fields::Unit => vec![],
@@ -49,11 +63,25 @@ pub fn derive_from_bytes(input: TokenStream) -> TokenStream {
                 .into();
         }
     };
-    let from_bytes_fields = fields.iter().map(|f| {
+    let from_bytes_fields = fields.iter_mut().map(|f| {
         let ident = f.ident.as_ref().unwrap();
-        let ty = &f.ty;
-        quote! {
-            #ident: #ty::from_bytes(buf)?
+        let ty = &mut f.ty;
+        let span = ty.span();
+        match ty {
+            syn::Type::Path(syn::TypePath { qself: None, path }) => {
+                if let Some(last) = path.segments.last_mut()
+                    && let syn::PathArguments::AngleBracketed(ab) = &mut last.arguments
+                    && ab.colon2_token.is_none()
+                {
+                    ab.colon2_token = Some(syn::Token![::](span));
+                }
+                quote! {
+                    #ident: #ty::from_bytes(buf)?
+                }
+            }
+            _ => quote! {
+                #ident: #ty::from_bytes(buf)?
+            },
         }
     });
     TokenStream::from(quote! {
